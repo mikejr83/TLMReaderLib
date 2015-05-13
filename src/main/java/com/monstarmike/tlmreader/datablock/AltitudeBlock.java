d4839c38d1f28bfe9d6c3b3db2339f8146125fd9
Mike Gardner <mikejr83@gmail.com>
2015-05-13 17:08:46 -0400

    adds in the altitude retreival.

--- src/main/java/com/monstarmike/tlmreader/datablock/AltitudeBlock.java
+++ src/main/java/com/monstarmike/tlmreader/datablock/AltitudeBlock.java
@@ -1,10 +1,25 @@
 package com.monstarmike.tlmreader.datablock;
 
-public final class AltitudeBlock extends DataBlock {
+import java.util.Arrays;
 
+import com.monstarmike.tlmreader.util.PrimitiveUtils;
+
+public final class AltitudeBlock extends DataBlock {
+/* 
+ Timestamp takes up the first 4 bytes
+ Address 0x12: Altitude
+
+ 12.00.MSB,LSB,FF,00,00,00,00,00,00,00,00,00,00,00
+ MSB and LSB are a 16Bit signed integer, 1 unit is 0.1m. Displays -3276.8m to 3276.7m, alarm values between -300m and 1000m. 
+ 0x7fff is 3276.7m, 0xffff=-0.1m, 0x8000 is -3276.8m
+ 
+ */
+	short altitudeInTenthsOfAMeter = 0;
+	
 	public AltitudeBlock(byte[] rawData) {
 		super(rawData);
-		// TODO Auto-generated constructor stub
+		
+		this.altitudeInTenthsOfAMeter = PrimitiveUtils.toShort(Arrays.copyOfRange(this.rawData, 6, 8));
 	}
 
 }
--- src/main/java/com/monstarmike/tlmreader/util/StringUtils.java
+++ src/main/java/com/monstarmike/tlmreader/util/StringUtils.java
@@ -2,13 +2,24 @@ package com.monstarmike.tlmreader.util;
 
 public class StringUtils {
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
 	public static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-	    }
-	    return new String(hexChars);
+		char[] hexChars = new char[bytes.length * 3];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 3] = hexArray[v >>> 4];
+			hexChars[j * 3 + 1] = hexArray[v & 0x0F];
+			hexChars[j * 3 + 2] = ' ';
+		}
+		return new String(hexChars);
+	}
+
+	public static String bytesToDec(byte[] bytes) {
+		String[] decParts = new String[bytes.length * 2];
+		for (int i = 0; i < bytes.length; i++) {
+			decParts[i * 2] = new Integer(bytes[i] & 0xff).toString();
+			decParts[i * 2 + 1] = " ";
+		}
+		return org.apache.commons.lang.StringUtils.join(decParts);
 	}
 }
