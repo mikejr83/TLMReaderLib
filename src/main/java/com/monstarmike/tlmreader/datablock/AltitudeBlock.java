ff5785670cf018c51344c6d101f7a5a7adfa764e
Mike Gardner <mikejr83@gmail.com>
2015-05-13 13:32:54 -0400

    Forgot to include a file.

---
+++ src/main/java/com/monstarmike/tlmreader/datablock/AltitudeBlock.java
@@ -0,0 +1,10 @@
+package com.monstarmike.tlmreader.datablock;
+
+public final class AltitudeBlock extends DataBlock {
+
+	public AltitudeBlock(byte[] rawData) {
+		super(rawData);
+		// TODO Auto-generated constructor stub
+	}
+
+}
