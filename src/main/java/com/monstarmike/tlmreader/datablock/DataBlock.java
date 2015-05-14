a85a0cbd3f08f3f291e78c133c296c8308737557
Mike Gardner <mikejr83@gmail.com>
2015-05-14 11:12:52 -0400

    timestamp calculation attempt

--- src/main/java/com/monstarmike/tlmreader/datablock/DataBlock.java
+++ src/main/java/com/monstarmike/tlmreader/datablock/DataBlock.java
@@ -1,63 +1,75 @@
 package com.monstarmike.tlmreader.datablock;
 
+import java.util.Arrays;
+import java.util.Date;
+
+import com.monstarmike.tlmreader.util.PrimitiveUtils;
+
 public abstract class DataBlock extends Block {
+
+	public int get_timestamp() {
+		byte[] timestampBytes = Arrays.copyOfRange(this.rawData, 0, 4);
+
+		org.apache.commons.lang.ArrayUtils.reverse(timestampBytes);
+		
+		return PrimitiveUtils.toInt(timestampBytes);
+	}
 
 	public DataBlock(byte[] rawData) {
 		super(rawData);
-		// TODO Auto-generated constructor stub
 	}
 
 	public static DataBlock createDataBlock(byte[] bytes) {
 		DataBlock block = null;
-		switch(bytes[4]) {
+		switch (bytes[4]) {
 		case 0:
-			//unknown
+			// unknown
 			break;
-			
+
 		case 0x01:
 			// voltmeter - appears to be legacy.
 			break;
-			
+
 		case 0x02:
 			// temperature - appears to be legacy.
 			break;
-			
+
 		case 0x03:
 			// current (amperage) usage
 			block = new CurrentBlock(bytes);
 			break;
-			
+
 		case 0x0a:
 			// powerbox
 			block = new PowerboxBlock(bytes);
 			break;
-		
+
 		case 0x11:
 			// airspeed
 			block = new AirspeedBlock(bytes);
 			break;
-			
+
 		case 0x12:
-			//altitude
+			// altitude
 			block = new AltitudeBlock(bytes);
 			break;
-			
+
 		case 0x14:
-			//gps
+			// gps
 			block = new GForceBlock(bytes);
 			break;
-			
+
 		case 126:
-			
+
 			break;
-			
+
 		case 127:
-			
+
 			break;
 		}
 		return block;
 	}
-	
+
 	@Override
 	public int getSize() {
 		// TODO Auto-generated method stub
