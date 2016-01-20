ac5f35593d910143601feb846ab7099a23656c79
David Leuenberger <david.leuenberger@gmx.ch>
2016-01-20 22:22:56 +0100

    Revert method names in StandardBlock.

--- src/main/java/com/monstarmike/tlmreader/datablock/StandardBlock.java
+++ src/main/java/com/monstarmike/tlmreader/datablock/StandardBlock.java
@@ -13,7 +13,7 @@ public class StandardBlock extends DataBlock {
 	private Double ratio;
 	private Byte poles;
 
-	public BigDecimal getRpm() {
+	public BigDecimal get_rpm() {
 		if (this.rpm == null) {
 			short rawRpmData = Shorts.fromBytes(this.rawData[6], this.rawData[7]);
 			if (rawRpmData == -1) { // 0xFFFF
@@ -26,7 +26,7 @@ public class StandardBlock extends DataBlock {
 		return this.rpm;
 	}
 
-	public BigDecimal getVolt() {
+	public BigDecimal get_volt() {
 		if (this.volt == null) {
 			this.volt = new BigDecimal(Shorts.fromBytes(this.rawData[8], this.rawData[9]) / 100).setScale(2,
 					RoundingMode.HALF_UP);
@@ -34,7 +34,11 @@ public class StandardBlock extends DataBlock {
 		return this.volt;
 	}
 
-	public BigDecimal getTemperatureInGradFahrenheit() {
+	public BigDecimal get_temperature() {
+		return get_temperatureInGradCelsius();
+	}
+
+	public BigDecimal get_temperatureInGradFahrenheit() {
 		if (this.tempInGradFahrenheit == null) {
 			this.tempInGradFahrenheit = new BigDecimal(Shorts.fromBytes(this.rawData[10], this.rawData[11])).setScale(2,
 					RoundingMode.HALF_UP);
@@ -42,9 +46,9 @@ public class StandardBlock extends DataBlock {
 		return this.tempInGradFahrenheit;
 	}
 
-	public BigDecimal getTemperatureInGradCelsius() {
+	public BigDecimal get_temperatureInGradCelsius() {
 		if (this.tempInGradFahrenheit == null) {
-			getTemperatureInGradFahrenheit();
+			get_temperatureInGradFahrenheit();
 		}
 		return new BigDecimal((this.tempInGradFahrenheit.doubleValue() - 32) / 1.8).setScale(2, RoundingMode.HALF_UP);
 	}
@@ -59,8 +63,8 @@ public class StandardBlock extends DataBlock {
 
 	@Override
 	public String toString() {
-		return super.toString() + "StandardData; RPM: " + this.getRpm() + ", Volt: " + this.getVolt()
-				+ ", Temperature (in °F): " + this.getTemperatureInGradFahrenheit() + ", Temperature (in °C): "
-				+ getTemperatureInGradCelsius();
+		return super.toString() + "StandardData; RPM: " + this.get_rpm() + ", Volt: " + this.get_volt()
+				+ ", Temperature (in °F): " + this.get_temperatureInGradFahrenheit() + ", Temperature (in °C): "
+				+ get_temperatureInGradCelsius();
 	}
 }
