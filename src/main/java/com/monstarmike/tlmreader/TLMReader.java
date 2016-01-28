3a2253fba683e470204d08e8b1e24111e8ec5d14
David Leuenberger <david.leuenberger@gmx.ch>
2016-01-28 19:45:58 +0100

    Use BufferedInputStream instead of ByteArray to reduce memory usage during parse.

--- src/main/java/com/monstarmike/tlmreader/TLMReader.java
+++ src/main/java/com/monstarmike/tlmreader/TLMReader.java
@@ -1,12 +1,13 @@
 package com.monstarmike.tlmreader;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 
-import com.google.common.io.Files;
 import com.monstarmike.tlmreader.datablock.DataBlock;
 import com.monstarmike.tlmreader.datablock.HeaderBlock;
 import com.monstarmike.tlmreader.datablock.HeaderDataBlock;
@@ -16,26 +17,27 @@ import com.monstarmike.tlmreader.datablock.HeaderVoltBlock;
 
 public class TLMReader implements Iterable<Flight> {
 	ArrayList<Flight> flights;
-	
+	private int byteCounter;
+
 	public TLMReader() {
 		flights = new ArrayList<Flight>();
 	}
-	
-	public void Read(String path) throws IOException {		
-		
-		byte[] theBytes = Files.toByteArray(new File(path));
-		
-		this.Read(theBytes);
+
+	public void Read(String path) throws IOException {
+		this.Read(new FileInputStream(new File(path)));
 	}
-	
-	public void Read(byte[] bytes) {
-		int i = 0;
+
+	public void Read(InputStream inputStream) throws IOException {
+		BufferedInputStream bufferedStream = new BufferedInputStream(inputStream);
 		Flight currentFlight = null;
-		while((i + 4) < bytes.length) {
-			byte[] headerTest = Arrays.copyOfRange(bytes, i, i + 4);
+		byte[] headerTest = new byte[4];
+		bufferedStream.mark(4);
+		while (bufferedStream.read(headerTest, 0, 4) == 4) {
+			bufferedStream.reset();
 			if (HeaderBlock.isHeaderBlock(headerTest)) {
-				byte[] headerBytes = Arrays.copyOfRange(bytes, i, i + 36);
-				if(HeaderNameBlock.isHeaderName(headerBytes)) {
+				byte[] headerBytes = new byte[36];
+				byteCounter += bufferedStream.read(headerBytes, 0, 36);
+				if (HeaderNameBlock.isHeaderName(headerBytes)) {
 					currentFlight = new Flight();
 					currentFlight.addBlock(new HeaderNameBlock(headerBytes));
 					this.flights.add(currentFlight);
@@ -46,18 +48,25 @@ public class TLMReader implements Iterable<Flight> {
 				} else {
 					currentFlight.addBlock(new HeaderDataBlock(headerBytes));
 				}
-				
-				i += 36;
-			}
-			else {
-				DataBlock db = DataBlock.createDataBlock(Arrays.copyOfRange(bytes, i, i + 20), currentFlight);
+			} else {
+				byte[] dataBytes = new byte[20];
+				byteCounter += bufferedStream.read(dataBytes, 0, 20);
+				DataBlock db = DataBlock.createDataBlock(dataBytes, currentFlight);
 				currentFlight.addBlock(db);
-				i += 20;	
 			}
+			bufferedStream.mark(4);
 		}
 	}
 
 	public Iterator<Flight> iterator() {
 		return this.flights.iterator();
+	}
+
+	public boolean hasFlights() {
+		return flights.size() > 0;
+	}
+
+	public int getNumberOfBytesRead() {
+		return byteCounter;
 	}
 }
