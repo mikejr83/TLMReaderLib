6f2ba7f2c3364e8e52d026a37fd1d7d1d81d3edd
David Leuenberger <david.leuenberger@gmx.ch>
2016-02-01 23:56:54 +0100

    Use primitives to reduce memory usage and increase performance

--- src/main/java/com/monstarmike/tlmreader/datablock/VarioBlock.java
+++ src/main/java/com/monstarmike/tlmreader/datablock/VarioBlock.java
@@ -4,67 +4,50 @@ import com.google.common.primitives.Shorts;
 
 public class VarioBlock extends DataBlock {
 
-	Short altitude = null, interval250 = null, interval500 = null,
-			interval1000 = null, interval2000 = null, interval3000 = null;
+	private short altitude, interval250, interval500, interval1000, interval2000, interval3000;
 
-	public Short get_altitude() {
-		if (this.altitude == null) {
-			this.altitude = Shorts.fromBytes(this.rawData[0x06],
-					this.rawData[0x07]);
-		}
-		return this.altitude;
+	public short get_altitude() {
+		return altitude;
 	}
 
-	public Short get_250ms() {
-		if (this.interval250 == null) {
-			this.interval250 = Shorts.fromBytes(this.rawData[0x08],
-					this.rawData[0x09]);
-		}
-		return this.interval250;
+	public short get_250ms() {
+		return interval250;
 	}
 
-	public Short get_500ms() {
-		if (this.interval500 == null) {
-			this.interval500 = Shorts.fromBytes(this.rawData[0x0A],
-					this.rawData[0x0B]);
-		}
-		return this.interval500;
+	public short get_500ms() {
+		return interval500;
 	}
 
-	public Short get_1000ms() {
-		if (this.interval1000 == null) {
-			this.interval1000 = Shorts.fromBytes(this.rawData[0x0C],
-					this.rawData[0x0D]);
-		}
-		return this.interval1000;
+	public short get_1000ms() {
+		return interval1000;
 	}
 
-	public Short get_2000ms() {
-		if (this.interval2000 == null) {
-			this.interval2000 = Shorts.fromBytes(this.rawData[0x0E],
-					this.rawData[0x0F]);
-		}
-		return this.interval2000;
+	public short get_2000ms() {
+		return interval2000;
 	}
 
-	public Short get_3000ms() {
-		if (this.interval3000 == null) {
-			this.interval3000 = Shorts.fromBytes(this.rawData[0x10],
-					this.rawData[0x11]);
-		}
-		return this.interval3000;
+	public short get_3000ms() {
+		return interval3000;
 	}
 
 	public VarioBlock(byte[] rawData) {
 		super(rawData);
+		decode(rawData);
+	}
+
+	private void decode(byte[] rawData) {
+		altitude = Shorts.fromBytes(rawData[0x06], rawData[0x07]);
+		interval250 = Shorts.fromBytes(rawData[0x08], rawData[0x09]);
+		interval500 = Shorts.fromBytes(rawData[0x0A], rawData[0x0B]);
+		interval1000 = Shorts.fromBytes(rawData[0x0C], rawData[0x0D]);
+		interval2000 = Shorts.fromBytes(rawData[0x0E], rawData[0x0F]);
+		interval3000 = Shorts.fromBytes(rawData[0x10], rawData[0x11]);
 	}
 
 	@Override
 	public String toString() {
-		return super.toString() + " - altitude: " + this.get_altitude()
-				+ " - 250 delta: " + this.get_250ms() + " - 500 delta: "
-				+ this.get_500ms() + " - 1000 delta: " + this.get_1000ms()
-				+ " - 2000 delta: " + this.get_2000ms() + " - 3000 delta: "
-				+ this.get_3000ms();
+		return super.toString() + " - altitude: " + get_altitude() + " - 250 delta: " + get_250ms() + " - 500 delta: "
+				+ get_500ms() + " - 1000 delta: " + get_1000ms() + " - 2000 delta: " + get_2000ms() + " - 3000 delta: "
+				+ get_3000ms();
 	}
 }
