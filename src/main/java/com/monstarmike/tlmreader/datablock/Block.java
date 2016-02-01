f5125e0d9e6ddf389dfcee588e4d2c3ab89e0c3a
David Leuenberger <david.leuenberger@gmx.ch>
2016-02-01 17:16:22 +0100

    Remove unused fields (reduce memory usage)

--- src/main/java/com/monstarmike/tlmreader/datablock/Block.java
+++ src/main/java/com/monstarmike/tlmreader/datablock/Block.java
@@ -4,31 +4,9 @@ import com.monstarmike.tlmreader.util.StringUtils;
 
 public abstract class Block {
 
-	byte[] rawTimestamp;
-	byte rawDataType;
-	byte rawFlag;
 	byte[] rawData;
 	
 	public abstract int getSize();
-	
-	public byte[] getRawTimestamp() {
-		return rawTimestamp;
-	}
-	public void setRawTimestamp(byte[] rawTimestamp) {
-		this.rawTimestamp = rawTimestamp;
-	}
-	public byte getRawDataType() {
-		return rawDataType;
-	}
-	public void setRawDataType(byte rawDataType) {
-		this.rawDataType = rawDataType;
-	}
-	public byte getRawFlag() {
-		return rawFlag;
-	}
-	public void setRawFlag(byte rawFlag) {
-		this.rawFlag = rawFlag;
-	}
 	
 	public Block(byte[] rawData) {
 		this.rawData = rawData;
