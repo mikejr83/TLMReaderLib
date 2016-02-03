d96e766f694257be0f86b4f7d25dedc42b9095ca
David Leuenberger <david.leuenberger@gmx.ch>
2016-02-03 17:09:20 +0100

    parse members during construction

--- src/main/java/com/monstarmike/tlmreader/datablock/HeaderNameBlock.java
+++ src/main/java/com/monstarmike/tlmreader/datablock/HeaderNameBlock.java
@@ -3,83 +3,64 @@ package com.monstarmike.tlmreader.datablock;
 import java.util.Arrays;
 
 public class HeaderNameBlock extends HeaderBlock {
-	String modelName = null;
-	int modelNumber = -1;
-	String modelType = null;
-	String bindInfo = null;
+	String modelName;
+	byte modelNumber;
+	byte modelType;
+	byte bindInfo;
 
 	public static boolean isHeaderName(byte[] bytes) {
 		return bytes.length > 6 && bytes[4] != bytes[5];
 	}
 
 	public String get_modelName() {
-		if (this.modelName == null) {
-			short offset = 0x0C;
-			while (offset < 0x24 && this.rawData[offset] != 0x0) {
-				offset++;
-			}
-			this.modelName = new String(Arrays.copyOfRange(this.rawData, 0x0C,
-					offset)).trim();
-		}
-
-		return this.modelName;
+		return modelName;
 	}
 
 	public int get_modelNumber() {
-		if (this.modelNumber < 0) {
-			this.modelNumber = ((short) this.rawData[0x4]) + 1;
-		}
-
-		return this.modelNumber;
+		return modelNumber;
 	}
 
 	public String get_modelType() {
-		if (this.modelType == null) {
-			switch (this.rawData[0x5]) {
-			case 0x0:
-				this.modelType = "Fixed Wing";
-				break;
-
-			case 0x1:
-				this.modelType = "Helicopter";
-				break;
-
-			case 0x2:
-				this.modelType = "Sailplane";
-				break;
-			}
+		switch (modelType) {
+		case 0x0:
+			return "Fixed Wing";
+		case 0x1:
+			return "Helicopter";
+		default:
+		case 0x2:
+			return "Sailplane";
 		}
-
-		return this.modelType;
 	}
 
 	public String get_bindInfo() {
-		if (this.bindInfo == null) {
-			switch (this.rawData[0x6]) {
-			case 0x1:
-				this.bindInfo = "DSM2 6000";
-				break;
-
-			case 0x2:
-				this.bindInfo = "DSM2 8000";
-				break;
-
-			case 0x3:
-				this.bindInfo = "DSMX 8000";
-				break;
-
-			case 0x4:
-				this.bindInfo = "DSMX 6000";
-				break;
-
-			}
+		switch (bindInfo) {
+		case 0x1:
+			return "DSM2 6000";
+		case 0x2:
+			return "DSM2 8000";
+		case 0x3:
+			return "DSMX 8000";
+		default:
+		case 0x4:
+			return "DSMX 6000";
 		}
-
-		return this.bindInfo;
 	}
 
 	public HeaderNameBlock(byte[] rawData) {
 		super(rawData);
+		decode(rawData);
+	}
+
+	private void decode(byte[] rawData) {
+		modelType = rawData[0x5];
+		modelType = rawData[0x5];
+		bindInfo = rawData[0x6];
+		short offset = 0x0C;
+		while (offset < 0x24 && rawData[offset] != 0x0) {
+			offset++;
+		}
+		modelName = new String(Arrays.copyOfRange(rawData, 0x0C, offset)).trim();
+		modelNumber = (byte) (((byte) rawData[0x4]) + 1);
 	}
 
 	@Override
