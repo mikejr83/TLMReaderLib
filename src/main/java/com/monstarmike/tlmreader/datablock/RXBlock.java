e02f890ba807fa649ccccf92dab41b87a14db476
David Leuenberger <david.leuenberger@gmx.ch>
2016-02-15 21:23:15 +0100

    Add interpretation of invalid values

--- src/main/java/com/monstarmike/tlmreader/datablock/RXBlock.java
+++ src/main/java/com/monstarmike/tlmreader/datablock/RXBlock.java
@@ -2,17 +2,34 @@ package com.monstarmike.tlmreader.datablock;
 
 import com.google.common.primitives.Shorts;
 
-
 /**
- * The Values of a, b, l ,r are number packetLoss of the each receiver. 
- * It looks like, that if the value is 0xFFFF or Short.MIN_VALUE there is no such receiver installed.
+ * <h4>Spectrum DSM2/DSMX Telemetry System - Data interpretation</h4> The Values
+ * of a, b, l ,r are number packetLoss of the each receiver. <br>
+ * It looks like, that Spectrum uses unsigned short data types, because the
+ * 0xffff (unsigned shortMaxValue) seems to be the value if no receiver is
+ * installed. I assume, that always the highest value of the datatype is the
+ * value for 'no data'.
+ * 
+ * <h4>Lemon DSM2/DSMX Telemetry Systen - Data interpretation</h4> However the
+ * Lemon unit does use the “A” display for a Signal Indicator value. This is
+ * calculated from the number of packets lost in transmission. A reading of 100
+ * means that no packets have been lost in transmission, while smaller numbers
+ * indicate losses.<br>
+ * The value is based on the number of packets received about every half second.
+ * <br>
+ * <code>Quote from 'docs/InstructionsTelemetry A4.pdf' , page 6, section
+ * "Accuracy and Limitations" subsection "Signal Indicator"</code> <br>
+ * <br>
+ * The values b,l,r,frameloss,holds are not used.<br>
+ * The not used values are 32768 (0x8000) == Short.MIN_VALUE.<br>
+ * It gives me the appearance, that Lemon is using signed short, with
+ * Short.MIN_VALUE for invalid values.
  */
 
 public class RXBlock extends DataBlock {
 
-	
-	private short a, b, l, r, frameLoss, holds;
-	private short voltageInHunderthOfVolts;
+	private short lostPacketsReceiverA, lostPacketsReceiverB, lostPacketsReceiverL, lostPacketsReceiverR, frameLoss,
+			holds, voltageInHunderthOfVolts;
 
 	public RXBlock(final byte[] rawData) {
 		super(rawData);
@@ -20,47 +37,113 @@ public class RXBlock extends DataBlock {
 	}
 
 	private void decode(final byte[] rawData) {
-		a = Shorts.fromBytes(rawData[0x06], rawData[0x07]);
-		b = Shorts.fromBytes(rawData[0x08], rawData[0x09]);
-		l = Shorts.fromBytes(rawData[0x0A], rawData[0x0B]);
-		r = Shorts.fromBytes(rawData[0x0C], rawData[0x0D]);
+		lostPacketsReceiverA = Shorts.fromBytes(rawData[0x06], rawData[0x07]);
+		lostPacketsReceiverB = Shorts.fromBytes(rawData[0x08], rawData[0x09]);
+		lostPacketsReceiverL = Shorts.fromBytes(rawData[0x0A], rawData[0x0B]);
+		lostPacketsReceiverR = Shorts.fromBytes(rawData[0x0C], rawData[0x0D]);
 		frameLoss = Shorts.fromBytes(rawData[0x0E], rawData[0x0F]);
 		holds = Shorts.fromBytes(rawData[0x10], rawData[0x11]);
 		voltageInHunderthOfVolts = Shorts.fromBytes(rawData[0x12], rawData[0x13]);
 	}
 
-	public short getA() {
-		return a;
+	private boolean isValidData(short value) {
+		return value >= 0;
 	}
 
-	public short getB() {
-		return b;
+	public boolean hasValidDataLostPacketsReceiverA() {
+		return isValidData(getLostPacketsReceiverA());
 	}
 
-	public short getL() {
-		return l;
+	/**
+	 * @return If Spectrum Telemetry System is used, the value shows
+	 *         LostDataPackets on the receiver.<br>
+	 *         If Lemon Telemetry System is used, the value is 100 if all is ok,
+	 *         less if packets are lost.
+	 */
+	public short getLostPacketsReceiverA() {
+		return lostPacketsReceiverA;
 	}
 
-	public short getR() {
-		return r;
+	public boolean hasValidDataLostPacketsReceiverB() {
+		return isValidData(getLostPacketsReceiverB());
 	}
 
+	/**
+	 * @return If Spectrum Telemetry System is used, the value shows
+	 *         LostDataPackets on the receiver.<br>
+	 *         If Lemon Telemetry System is used, the value is not used
+	 */
+	public short getLostPacketsReceiverB() {
+		return lostPacketsReceiverB;
+	}
+
+	public boolean hasValidDataLostPacketsReceiverL() {
+		return isValidData(getLostPacketsReceiverL());
+	}
+
+	/**
+	 * @return If Spectrum Telemetry System is used, the value shows
+	 *         LostDataPackets on the receiver.<br>
+	 *         If Lemon Telemetry System is used, the value is not used
+	 */
+	public short getLostPacketsReceiverL() {
+		return lostPacketsReceiverL;
+	}
+
+	public boolean hasValidDataLostPacketsReceiverR() {
+		return isValidData(getLostPacketsReceiverR());
+	}
+
+	/**
+	 * @return If Spectrum Telemetry System is used, the value shows
+	 *         LostDataPackets on the receiver.<br>
+	 *         If Lemon Telemetry System is used, the value is not used
+	 */
+	public short getLostPacketsReceiverR() {
+		return lostPacketsReceiverR;
+	}
+
+	public boolean hasValidFrameLosssData() {
+		return isValidData(getFrameLoss());
+	}
+
+	/**
+	 * @return If Spectrum Telemetry System is used, the value shows
+	 *         lost frames.<br>
+	 *         If Lemon Telemetry System is used, the value is not used.
+	 */
 	public short getFrameLoss() {
 		return frameLoss;
 	}
 
+	public boolean hasValidHoldsData() {
+		return isValidData(getHolds());
+	}
+
+	/**
+	 * @return If Spectrum Telemetry System is used, the value shows
+	 *         holds.<br>
+	 *         If Lemon Telemetry System is used, the value is not used.
+	 */
 	public short getHolds() {
 		return holds;
 	}
 
+	/**
+	 * @return Returns the voltage in hunderth of Volt of the receiver.
+	 */
 	public short getVoltageInHunderthOfVolts() {
 		return voltageInHunderthOfVolts;
 	}
 
 	@Override
 	public String toString() {
-		return super.toString() + "RxData; FrameLoss: " + getFrameLoss() + ", Holds: " + getHolds() + ", Volts: "
-				+ getVoltageInHunderthOfVolts();
+		return "RxData; LostPacketsReceiver A: " + getLostPacketsReceiverA() + " (" + hasValidDataLostPacketsReceiverA()
+				+ "), B: " + getLostPacketsReceiverB() + " (" + hasValidDataLostPacketsReceiverB() + "), L: "
+				+ getLostPacketsReceiverL() + " (" + hasValidDataLostPacketsReceiverL() + "), R: "
+				+ getLostPacketsReceiverR() + " (" + hasValidDataLostPacketsReceiverR() + "), FrameLoss: "
+				+ getFrameLoss() + "(" + hasValidFrameLosssData() + ") , Holds: " + getHolds() + "("
+				+ hasValidHoldsData() + "), Volts: " + getVoltageInHunderthOfVolts();
 	}
 
 }
--- src/test/java/com/monstarmike/tlmreader/datablock/RXBlockTest.java
+++ src/test/java/com/monstarmike/tlmreader/datablock/RXBlockTest.java
@@ -1,6 +1,8 @@
 package com.monstarmike.tlmreader.datablock;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -11,44 +13,73 @@ public class RXBlockTest {
 	private static final short RX_DATABLOCK_MAKER = (short) 0x7F00;
 
 	@Test
-	public void testValueA() {
+	public void testGetLostPacketsReceiverA() {
 		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(123, 0x06).get();
 		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
-		assertEquals(123, (short) rxBlock.getA());
+		assertEquals(123, (short) rxBlock.getLostPacketsReceiverA());
+	}
+	
+	@Test
+	public void testHasValidDataLostPacketsReceiverAUnsignedShortMax() {
+		int unsignedShortMax = 0xFFFF;
+		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(unsignedShortMax, 0x06).get();
+		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
+		assertFalse(rxBlock.hasValidDataLostPacketsReceiverA());
+	}
+	
+	@Test
+	public void testHasValidDataLostPacketsReceiverASignedShortMax() {
+		int signedShortMax = Short.MAX_VALUE+1;
+		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(signedShortMax, 0x06).get();
+		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
+		assertFalse(rxBlock.hasValidDataLostPacketsReceiverA());
+	}
+	
+	@Test
+	public void testHasValidDataLostPacketsReceiverAOk() {
+		int signedShortMax = 0;
+		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(signedShortMax, 0x06).get();
+		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
+		assertTrue(rxBlock.hasValidDataLostPacketsReceiverA());
 	}
 
 	@Test
-	public void testValueB() {
+	public void testGetLostPacketsReceiverB() {
 		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(739, 0x08).get();
 		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
-		assertEquals(739, (short) rxBlock.getB());
+		assertTrue(rxBlock.hasValidDataLostPacketsReceiverB());
+		assertEquals(739, (short) rxBlock.getLostPacketsReceiverB());
 	}
 
 	@Test
-	public void testValueL() {
+	public void testGetLostPacketsReceiverL() {
 		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(999, 0x0A).get();
 		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
-		assertEquals(999, (short) rxBlock.getL());
+		assertTrue(rxBlock.hasValidDataLostPacketsReceiverL());
+		assertEquals(999, (short) rxBlock.getLostPacketsReceiverL());
 	}
 
 	@Test
-	public void testValueR() {
+	public void testGetLostPacketsReceiverR() {
 		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(302, 0x0C).get();
 		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
-		assertEquals(302, (short) rxBlock.getR());
+		assertTrue(rxBlock.hasValidDataLostPacketsReceiverR());
+		assertEquals(302, (short) rxBlock.getLostPacketsReceiverR());
 	}
 
 	@Test
-	public void testFrameLoss() {
+	public void testGetFrameLoss() {
 		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(3, 0x0E).get();
 		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
+		assertTrue(rxBlock.hasValidFrameLosssData());
 		assertEquals(3, (short) rxBlock.getFrameLoss());
 	}
 
 	@Test
-	public void testHolds() {
+	public void testGetHolds() {
 		byte[] testBlock = new DataBlockBuilder(0, RX_DATABLOCK_MAKER).setValue(15, 0x10).get();
 		RXBlock rxBlock = (RXBlock) DataBlock.createDataBlock(testBlock, null);
+		assertTrue(rxBlock.hasValidHoldsData());
 		assertEquals(15, (short) rxBlock.getHolds());
 	}
 
