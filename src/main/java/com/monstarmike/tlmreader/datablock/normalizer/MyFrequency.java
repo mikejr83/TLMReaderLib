d66f1111a8749cbc0b942d2ae5d09562e52eaef0
David Leuenberger <david.leuenberger@gmx.ch>
2016-02-17 07:50:13 +0100

    similar implementation of Frequency from commons-math3, no dependency

---
+++ src/main/java/com/monstarmike/tlmreader/datablock/normalizer/MyFrequency.java
@@ -0,0 +1,53 @@
+package com.monstarmike.tlmreader.datablock.normalizer;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+
+/**
+ * This class implements a subset of similar methods inspired from the Frequency
+ * class from org.apache.commons-math3. <br>
+ * The aim of this own implementation is, to not include the whole commons-math3
+ * library.
+ */
+public class MyFrequency {
+
+	private Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
+	private int totalCount = 0;
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+	
+	public void addValue(int value) {
+		Integer key = Integer.valueOf(value);
+		int count = 1;
+		if (map.containsKey(key)) {
+			count += map.get(key).intValue();
+		}
+		map.put(key, count);
+		totalCount++;
+	}
+
+	public Iterator<Entry<Integer, Integer>> entrySetIterator() {
+		return map.entrySet().iterator();
+	}
+
+	public int getCumFreq(Integer v) {
+		int cumulativCount = 0;
+		for (Entry<Integer, Integer> entry : map.entrySet()) {
+			if (entry.getKey() <= v) {
+				cumulativCount += entry.getValue();
+			} else {
+				return cumulativCount;
+			}
+		}
+		return cumulativCount;
+	}
+
+	public double getCumPct(Integer v) {
+		return (double) getCumFreq(v) / totalCount;
+	}
+}
---
+++ src/test/java/com/monstarmike/tlmreader/datablock/normalizer/MyFrequencyTest.java
@@ -0,0 +1,73 @@
+package com.monstarmike.tlmreader.datablock.normalizer;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyFrequencyTest {
+
+	private MyFrequency frequency;
+
+	@Before
+	public void setup() {
+		frequency = new MyFrequency();
+		frequency.addValue(2);
+		frequency.addValue(2);
+		frequency.addValue(1);
+		frequency.addValue(1);
+		frequency.addValue(1);
+		frequency.addValue(1);
+		frequency.addValue(2);
+		frequency.addValue(5);
+	}
+
+	@Test
+	public void testAddValue() {
+		Assert.assertEquals(8, frequency.getTotalCount());
+	}
+
+	@Test
+	public void testEntrySetIterator() {
+		Iterator<Entry<Integer, Integer>> entrySetIterator = frequency.entrySetIterator();
+		Assert.assertTrue(entrySetIterator.hasNext());
+		Entry<Integer, Integer> next = entrySetIterator.next();
+		Assert.assertEquals(1, next.getKey().intValue());
+		Assert.assertEquals(4, next.getValue().intValue());
+
+		Assert.assertTrue(entrySetIterator.hasNext());
+		next = entrySetIterator.next();
+		Assert.assertEquals(2, next.getKey().intValue());
+		Assert.assertEquals(3, next.getValue().intValue());
+
+		Assert.assertTrue(entrySetIterator.hasNext());
+		next = entrySetIterator.next();
+		Assert.assertEquals(5, next.getKey().intValue());
+		Assert.assertEquals(1, next.getValue().intValue());
+	}
+
+	@Test
+	public void testGetCumFreq() {
+		Assert.assertEquals(0, frequency.getCumFreq(0));
+		Assert.assertEquals(4, frequency.getCumFreq(1));
+		Assert.assertEquals(7, frequency.getCumFreq(2));
+		Assert.assertEquals(7, frequency.getCumFreq(3));
+		Assert.assertEquals(7, frequency.getCumFreq(4));
+		Assert.assertEquals(8, frequency.getCumFreq(5));
+		Assert.assertEquals(8, frequency.getCumFreq(6));
+	}
+	
+	@Test
+	public void testGetCumPct() {
+		Assert.assertEquals(0.0, frequency.getCumPct(0), 0.001);
+		Assert.assertEquals(0.5, frequency.getCumPct(1), 0.001);
+		Assert.assertEquals(0.875, frequency.getCumPct(2), 0.001);
+		Assert.assertEquals(0.875, frequency.getCumPct(3), 0.001);
+		Assert.assertEquals(0.875, frequency.getCumPct(4), 0.001);
+		Assert.assertEquals(1, frequency.getCumPct(5), 0.001);
+		Assert.assertEquals(1, frequency.getCumPct(6), 0.001);
+	}
+
+}
