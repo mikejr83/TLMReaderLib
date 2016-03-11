d7b33d599e698ff6209d050e2bf3b58399baef93
David Leuenberger <david.leuenberger@gmx.ch>
2016-03-11 20:49:40 +0100

    add javadoc

--- src/main/java/com/monstarmike/tlmreader/TLMReader.java
+++ src/main/java/com/monstarmike/tlmreader/TLMReader.java
@@ -16,22 +16,78 @@ import com.monstarmike.tlmreader.datablock.HeaderRpmBlock;
 import com.monstarmike.tlmreader.datablock.HeaderRxBlock;
 import com.monstarmike.tlmreader.datablock.HeaderVoltBlock;
 
+/**
+ * This Class is responsible for parsing TLM files. <br>
+ * The usage of this class is:<br>
+ * <ul>
+ * <li>First parse the whole TLM file and returns a list file
+ * {@link FlightDefinition} s with the method 'parseFlightDefinitions'. The
+ * {@link FlightDefinition} contains information about the flight with
+ * HeaderBlocks, but without datablocks.</li>
+ * <li>Second get the all details of a specific {@link Flight} with a call of
+ * the method 'parseFlight'.</li>
+ * </ul>
+ */
 public class TLMReader {
-	
+
 	private int byteCounter;
 
+	/**
+	 * @return The size of the TLM file.
+	 */
 	public int getNumberOfBytesRead() {
 		return byteCounter;
 	}
 
+	/**
+	 * The method parse the given file and returns a lists of {@link IFlight}
+	 * s. <br>
+	 * To reduce the memory usage with large TLM files, the {@link IFlight}
+	 * Interface is used with lightweight {@link FlightDefinition} instances.
+	 * 
+	 * @param path
+	 *               The path of the file to parse.
+	 * @return A list of {@link IFlight} objects (they do no contain
+	 *         dataBlocks
+	 * @throws IOException
+	 *                If the file could not be read successfully, then a
+	 *                {@link IOException} is thrown.
+	 */
 	public List<IFlight> parseFlightDefinitions(String path) throws IOException {
 		return this.parseFlightDefinitions(new FileInputStream(new File(path)));
 	}
 
-	public Flight parseFlight(String path, int joId) throws IOException {
-		return this.parseFlight(new FileInputStream(new File(path)), joId);
+	/**
+	 * Reads the specified flight with all details from a given file.
+	 * 
+	 * @param path
+	 *               The path of the file to parse.
+	 * @param index
+	 *               The index of the flight which should be read. The first
+	 *               flight has the index 0.
+	 * @return The requested {@link Flight} with all details
+	 * @throws IOException
+	 *                If the file could not be read successfully, then a
+	 *                {@link IOException} is thrown.
+	 */
+	public Flight parseFlight(String path, int index) throws IOException {
+		return this.parseFlight(new FileInputStream(new File(path)), index);
 	}
 
+	/**
+	 * The method parse the given {@link InputStream} and returns a lists of
+	 * {@link IFlight}s. <br>
+	 * To reduce the memory usage with large TLM files, the {@link IFlight}
+	 * Interface is used with lightweight {@link FlightDefinition} instances.
+	 * 
+	 * @param inputStream
+	 *               The {@link InputStream} to parse.
+	 * @return A list of {@link IFlight} objects (they do no contain
+	 *         dataBlocks
+	 * @throws IOException
+	 *                If the file could not be read successfully, then a
+	 *                {@link IOException} is thrown.
+	 */
 	public List<IFlight> parseFlightDefinitions(InputStream inputStream) throws IOException {
 		final ArrayList<IFlight> flights = new ArrayList<IFlight>();
 		new TlmParser() {
@@ -48,7 +104,8 @@ public class TLMReader {
 
 			@Override
 			public void handleDataBlock(byte[] dataBytes, int flightNumber) {
-				DataBlock createdDataBlock = DataBlock.createDataBlock(dataBytes, getCurrentFlight().getRpmHeader());
+				DataBlock createdDataBlock = DataBlock.createDataBlock(dataBytes,
+						getCurrentFlight().getRpmHeader());
 				if (createdDataBlock != null) {
 					getCurrentFlight().addDataBlock(createdDataBlock);
 				}
@@ -64,13 +121,27 @@ public class TLMReader {
 		return flights;
 	}
 
-	public Flight parseFlight(InputStream inputStream, final int joId) throws IOException {
+	/**
+	 * Reads the specified flight with all details from a given
+	 * {@link InputStream}.
+	 * 
+	 * @param inputStream
+	 *               The {@link InputStream} to parse.
+	 * @param index
+	 *               The index of the flight which should be read. The first
+	 *               flight has the index 0.
+	 * @return The requested {@link Flight} with all details
+	 * @throws IOException
+	 *                If the file could not be read successfully, then a
+	 *                {@link IOException} is thrown.
+	 */
+	public Flight parseFlight(InputStream inputStream, final int index) throws IOException {
 		final ArrayList<Flight> flights = new ArrayList<Flight>();
 		new TlmParser() {
 
 			@Override
 			public void handleHeaderBlock(byte[] headerBytes, int flightNumber) {
-				if (flightNumber != joId) {
+				if (flightNumber != index) {
 					return;
 				}
 				if (HeaderNameBlock.isHeaderName(headerBytes)) {
@@ -89,10 +160,11 @@ public class TLMReader {
 
 			@Override
 			public void handleDataBlock(byte[] dataBytes, int flightNumber) {
-				if (flightNumber != joId) {
+				if (flightNumber != index) {
 					return;
 				}
-				DataBlock createdDataBlock = DataBlock.createDataBlock(dataBytes, getCurrentFlight().getRpmHeader());
+				DataBlock createdDataBlock = DataBlock.createDataBlock(dataBytes,
+						getCurrentFlight().getRpmHeader());
 				if (createdDataBlock != null) {
 					getCurrentFlight().addDataBlock(createdDataBlock);
 				}
@@ -106,7 +178,7 @@ public class TLMReader {
 			}
 		}.parseStream(new BufferedInputStream(inputStream));
 		if (flights.isEmpty()) {
-			throw new RuntimeException("FlightId  '"+joId+"' not found in inputStream.");
+			throw new RuntimeException("FlightId  '" + index + "' not found in inputStream.");
 		}
 		Flight flight = flights.get(flights.size() - 1);
 		flight.normalizeDataBlocks();
